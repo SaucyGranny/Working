1f0a47abab1585666d0ca61e2567558ce9c56e5d
SaucyGranny <67840491+SaucyGranny@users.noreply.github.com>
2021-03-17 18:11:20 -0400

    Update get_data.py

--- app/utils/get_data.py
+++ app/utils/get_data.py
@@ -52,13 +52,13 @@ def get_data_daily_reports_us() -> pd.DataFrame:
     latest_base_url = helper_get_latest_data_url(JHU_CSSE_FILE_PATHS['BASE_URL_DAILY_REPORTS_US'])
 
     # Extract the data
-    df = pd.read_csv(latest_base_url)
+    self.df = pd.read_csv(latest_base_url)
 
     # Data pre-processing
     concerned_columns = ['Confirmed', 'Deaths', 'Recovered', 'Active']
-    df = helper_df_cols_cleaning(df, concerned_columns, int)
+    self.df = helper_df_cols_cleaning(df, concerned_columns, int)
     
-    return df
+    return self.df
 
 
 # Get data from time series
